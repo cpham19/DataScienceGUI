84d817d1aed5aa550220ff3e9e3cd158e71bf052
Calvin Pham <calvinthepham@gmail.com>
2018-09-22 14:25:15 -0700

    first commit

---
+++ GUI.py
@@ -0,0 +1,15 @@
+##Import of Tkinter module
+import tkinter
+from tkinter import *
+
+##Creates the window from the imported Tkinter module
+window = Tk()
+
+##Creates the size of the window
+window.geometry("600x400")
+
+##Adds a title to the Windows GUI for the window
+window.title("Data Science GUI")
+
+##Loops the window to prevent the window from just "flashing once"
+window.mainloop()
